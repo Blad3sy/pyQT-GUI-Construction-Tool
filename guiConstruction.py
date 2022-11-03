97f14507e8890359c374e7a532bc2593ffd7fbae
Bladesy <oscarprosser@gmail.com>
2022-11-03 12:46:22 +0000

    Optimised Window() and Label()
    - Fixed Issue #1 (formatting is now consistent)
    
    - Window objects now automatically call show() when created
    
    - Label objects now automatically call show() when created so they can be dynamically added to already shown Windows - works with new Window() object implementation

--- guiConstruction.py
+++ guiConstruction.py
@@ -1,5 +1,3 @@
-from http.client import PARTIAL_CONTENT
-from multiprocessing import parent_process
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 import sys
@@ -16,21 +14,27 @@ class Window(QMainWindow):
 
         self.xPos = 200
         self.yPos = 200
-        self.width = 300
-        self.height = 300
+        self.width = 600
+        self.height = 600
         self.title = "Test"
 
         self.setGeometry(self.xPos, self.yPos, self.width, self.height)
         self.setWindowTitle(self.title)
 
+        self.show()
+
 class Label(QLabel):
-    def __init__(self, parent):
+    
+    def __init__(self, parent, text, xPos, yPos):
         QLabel.__init__(self, parent)
 
-        self.xPos = 50
-        self.yPos = 50
+        self.xPos = xPos
+        self.yPos = yPos
         self.width = None
         self.height = None
-        self.text = "Test"
+        self.text = text
 
+        self.move(self.xPos, self.yPos)
         self.setText(self.text)
+
+        self.show()
--- testing.py
+++ testing.py
@@ -2,8 +2,11 @@ from guiConstruction import Application, Window, Label
 
 mainApp = Application()
 mainWin = Window()
+subWin = Window()
+sub2Win = Window()
 
-label = Label(mainWin)
+label = Label(mainWin, "test", 50, 50)
+label2 = Label(subWin, "test2", 100, 100)
+label3 = Label(sub2Win, "chromebooks", 300, 300)
 
-mainWin.show()
 mainApp.exit(mainApp.exec_())
